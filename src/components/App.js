801462b1b6162e4dc914a26ec1bd46741e052b96
Tlangelani Mboweni <mbowenit@gmail.com>
2017-04-24 17:00:54 +0200

    add App component

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+    render() {
+        return (
+            <div className="component-app">
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit tenetur accusamus ea.</p>
+            </div>
+        );
+    }
+}
+
+export default App;
+
--- src/index.js
+++ src/index.js
@@ -1,1 +1,5 @@
-console.log('REACT & REDUX using Express');
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+import App from './components/App';
+
+render(<App />, document.getElementById('app'));
